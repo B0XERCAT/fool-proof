13678609062a15f997f75011b8201bb88f2f889c
b0xercat <seojin3154@naver.com>
2024-08-04 18:47:49 +0900

    chore: install prettier tailwindcss plugin, execute prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return <Button>Fool Proof!</Button>;
+  return <Button>Fool Proof!</Button>
 }
