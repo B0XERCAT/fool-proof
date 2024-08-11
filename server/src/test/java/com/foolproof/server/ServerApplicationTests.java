411445ca68bd13f5aa335974cfbc4576373d274f
paulpeterkim <paulpeterkim@gmail.com>
2024-08-11 19:28:33 +0900

    feat(be): make spring boot scaffolding

---
+++ server/src/test/java/com/foolproof/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foolproof.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
