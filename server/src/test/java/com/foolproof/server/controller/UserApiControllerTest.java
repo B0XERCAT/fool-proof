a64bae7ea87845936eb41465a3a95c82f52a3c80
paulpeterkim <paulpeterkim@gmail.com>
2024-08-21 23:59:13 +0900

    feat(be): creat test for user api controller

--- server/src/test/java/com/foolproof/server/controller/UserApiControllerTest.java
+++ server/src/test/java/com/foolproof/server/controller/UserApiControllerTest.java
@@ -2,15 +2,27 @@ package com.foolproof.server.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.foolproof.server.domain.User;
+import com.foolproof.server.dto.UserDTO;
 import com.foolproof.server.repository.UserRepository;
+
+import static org.springframework.test.web.servlet.result.MockMvcBuilders.status;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcBuilders.post;
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -35,4 +47,29 @@ public class UserApiControllerTest {
     }
 
     @DisplayName("addUser: Succeed adding user.")
+    @Test
+    public void addUser() throws Exception {
+        // Given
+        final String url = "/test/user/add";
+        final String username = "testUser";
+        final String password = "testPass";
+        final UserDTO userRequest = new UserDTO(username, password);
+
+        final String requestBody = objectMapper.writeValueAsString(userRequest);
+
+        // When
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        // Then
+        result.addExpect(status().isCreated());
+
+        List<User> users = userRepository.findAll();
+
+        assertThat(users.size()).isEqualTo(1);
+        User firstUser = users.get(0);
+        assertThat(firstUser.getUsername()).isEqualTo(username);
+        assertThat(firstUser.getPassword()).isEqualTo(password);
+    }
 }
