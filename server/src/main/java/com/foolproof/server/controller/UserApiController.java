deb979607f7f78ed4063d2927fbf0b33a43b0d17
b0xercat <seojin3154@naver.com>
2024-08-28 01:16:05 +0900

    fix(be): edit user controller endpoint

--- server/src/main/java/com/foolproof/server/controller/UserApiController.java
+++ server/src/main/java/com/foolproof/server/controller/UserApiController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.foolproof.server.domain.User;
@@ -14,10 +15,11 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class UserApiController {
     private final UserService userService;
 
-    @PostMapping("/test/user/add")
+    @PostMapping("/users")
     public ResponseEntity<User> addUser(@RequestBody UserDTO request) {
         User savedUser = userService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
