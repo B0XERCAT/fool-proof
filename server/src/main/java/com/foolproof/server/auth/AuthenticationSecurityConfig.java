8e590d678ffbcd818d0e3c481eb42a58a81498ca
Paul Kim <paulpeterkim@gmail.com>
2024-09-03 20:09:17 +0900

    feat (be): add config file for spring security

---
+++ server/src/main/java/com/foolproof/server/auth/AuthenticationSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.foolproof.server.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class AuthenticationSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(
+                (auth) -> auth
+                    .requestMatchers("/").authenticated()
+                    .requestMatchers("/api").permitAll()
+                    .anyRequest().autheticated()
+            );
+
+        return http.build();
+    }
+
+}
