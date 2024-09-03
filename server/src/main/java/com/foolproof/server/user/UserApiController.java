4f55c35e871b59671c6594f2d3d87760551f1213
Paul Kim <paulpeterkim@gmail.com>
2024-09-03 11:29:03 +0900

    refactor (be): undo renaming from UserAPIController to UserApiController

--- server/src/main/java/com/foolproof/server/user/UserApiController.java
+++ server/src/main/java/com/foolproof/server/user/UserApiController.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
-public class UserAPIController {
+public class UserApiController {
     private final UserService userService;
 
     @PostMapping("/users")
