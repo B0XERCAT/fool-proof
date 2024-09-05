0f16bdb22671451ed65ffc4c513eadb4194e63b5
paulpeterkim <paulpeterkim@gmail.com>
2024-09-05 14:03:54 +0900

    feat (be): implement username check in UserRepository

--- server/src/main/java/com/foolproof/domain/user/UserRepository.java
+++ server/src/main/java/com/foolproof/domain/user/UserRepository.java
@@ -3,5 +3,6 @@ package com.foolproof.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
+    Boolean existsByUsername(String username);
 }
