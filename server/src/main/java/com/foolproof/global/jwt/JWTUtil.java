c400082608bc3dae005dbb958f87be275b77bbee
paulpeterkim <paulpeterkim@gmail.com>
2024-09-05 23:18:22 +0900

    feat (be): add class for jwt issuance and verification

---
+++ server/src/main/java/com/foolproof/global/jwt/JWTUtil.java
@@ -0,0 +1,71 @@
+package com.foolproof.global.jwt;
+
+import static java.util.Collections.rotate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JWTUtil {
+
+    private final SecretKey secretKey;
+
+    public JWTUtil(@Value("${SPRING_JWT_SECRET}") String secretKey) {
+        this.secretKey = new SecretKeySpec(
+            secretKey.getBytes(StandardCharsets.UTF_8),
+            Jwts.SIG.HS256
+                .key()
+                .build()
+                .getAlgorithm()
+        );
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .getExpiration()
+            .before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {
+        Long curTimeMils = System.currentTimeMillis();
+        Date curTime = new Date(curTimeMils);
+        Date expTime = new Date(curTimeMils + expiredMs);
+        return Jwts.builder()
+            .claim("username", username)
+            .claim("role", role)
+            .issuedAt(curTime)
+            .expiration(expTime)
+            .signWith(secretKey)
+            .compact();
+    }
+}
