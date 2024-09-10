dcbf8504958533c05371986454e1277672faf2ba
paulpeterkim <paulpeterkim@gmail.com>
2024-09-10 23:19:22 +0900

    fix (be): fix an error where deleted refresh token being added back to the redis db and deleted unnecessary method

--- server/src/main/java/com/foolproof/global/jwt/ReissueService.java
+++ server/src/main/java/com/foolproof/global/jwt/ReissueService.java
@@ -34,6 +34,16 @@ public class ReissueService {
     }
 
     public Pair<String, Cookie> onSuccess(String refresh) {
+        /*
+         * From given token, create new access and refresh token.
+         * Then, return access token and "cookie-fied" refresh token.
+         * 
+         * @param refresh String representation of old refresh token
+         * 
+         * @return        An instance of Pair of String and Cookie.
+         *                An instance of string (the first in pair) is new access token.
+         *                An instance of Cookie (the second in pair) is new refresh token.
+         */
         // Parse username and password from refresh token
         String username = jwtUtil.getUsername(refresh);
         String role = jwtUtil.getRole(refresh);
@@ -45,24 +55,27 @@ public class ReissueService {
         );
         
         // Create new refresh token
-        Cookie newRefreshToken = (Cookie) getNewRefreshToken(
+        String newRefreshToken = getNewRefreshToken(
             username,
-            role,
-            true
+            role
         );
+
+        // Create new cookie from new refresh token
+        Cookie newRefreshTokenCookie = toCookie("refresh", newRefreshToken);
 
         // Delete old refresh token and add new one
         deleteByRefreshToken(refresh);
-        addRefreshToken(refresh);
+        addRefreshToken(newAccessToken);
 
         return Pair.of(
             newAccessToken,
-            newRefreshToken
+            newRefreshTokenCookie
         );
     }
 
     public String getNewAccessToken(String refresh) {
-        /* Get String representation of newly created access token
+        /* 
+         * Get String representation of newly created access token
          * from already existing refresh token.
          * 
          * @param refresh String representation of old refresh token.
@@ -77,7 +90,8 @@ public class ReissueService {
     }
 
     public String getNewAccessToken(String username, String role) {
-        /* Get String representation of newly created access token
+        /* 
+         * Get String representation of newly created access token
          * from username and role.
          * 
          * @param username String representation of username.
@@ -93,7 +107,8 @@ public class ReissueService {
     }
 
     public String getNewRefreshToken(String refresh) {
-        /* Get String representation of newly created refresh token
+        /* 
+         * Get String representation of newly created refresh token
          * from already existing refresh token.
          * 
          * @param refresh String representation of old refresh token.
@@ -108,7 +123,8 @@ public class ReissueService {
     }
 
     public String getNewRefreshToken(String username, String role) {
-        /* Get String representation of newly created refresh token
+        /* 
+         * Get String representation of newly created refresh token
          * from username and role.
          * 
          * @param username String representation of username.
@@ -123,36 +139,22 @@ public class ReissueService {
         );
     }
 
-    public Object getNewRefreshToken(String refresh, Boolean asCookie) {
-        String token = getNewRefreshToken(refresh);
-        if (asCookie){
-            Cookie cookie = new Cookie(
-                "refresh", 
-                token
-            );
-            cookie.setMaxAge(24 * 60 * 60);
-            // cookie.setSecure(true);
-            // cookie.setPath("/");
-            cookie.setHttpOnly(true);
-            return cookie;
-        } else {
-            return token;
-        }
+    public Cookie toCookie(String key, String value) {
+        Cookie cookie = new Cookie(
+            key, 
+            value
+        );
+        cookie.setMaxAge(24 * 60 * 60);
+        // cookie.setSecure(true);
+        // cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        return cookie;
     }
-
     
     public Object getNewRefreshToken(String username, String role, Boolean asCookie) {
         String token = getNewRefreshToken(username, role);
         if (asCookie){
-            Cookie cookie = new Cookie(
-                "refresh", 
-                token
-            );
-            cookie.setMaxAge(24 * 60 * 60);
-            // cookie.setSecure(true);
-            // cookie.setPath("/");
-            cookie.setHttpOnly(true);
-            return cookie;
+            return toCookie("refresh", token);
         } else {
             return token;
         }
